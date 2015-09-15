741e02220ab72d396d141e1b95055d85af12b465
hanimal <hanimal@bitbucket.org>
2015-09-14 21:19:04 -0300

    [master] Para el filtro blur: Intenté no modificar todo el código y me menejé con lo que tenía. Me acerqué algo pero aun así no es lo que queremos.

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -18,21 +18,23 @@ void blur_c    (
     int i = radius;
 	while(i< (filas - radius)){
 		// printf ("i :%i \n" , i);
-		int j = radius;
+		int j = radius*4;
 		while(j < (cols -radius) *4 ){
 			// printf ("j :%i \n" , j);
 			int promedio_r = 0;
 			int promedio_g = 0;
 			int promedio_b = 0;
 			int aux_i = i - radius;
-			int aux_j = j - radius;
-			while(aux_i < (i + radius) ){
-				while(aux_j <  (j + (4*radius)) ) {
+			int aux_j = j - radius*4;
+			while(aux_i <= (i + radius) ){
+				//int aux_j = j - radius*4;
+				while(aux_j <= ( j + radius*4 ) ) {
 					bgra_t *src_pixel = (bgra_t*)&src_matrix[aux_i][aux_j];
 					promedio_r += src_pixel->r;
 					promedio_g += src_pixel->g;
 					promedio_b += src_pixel->b;
-					aux_j++;
+					//aux_j = aux_j + 4;
+					aux_j += 4;
 				}
 				aux_i++;
 			}
@@ -47,10 +49,27 @@ void blur_c    (
 			dst_pixel->g = promedio_g;
 			dst_pixel->b = promedio_b;
 			dst_pixel->a = 255;
-			j++;
+			//j = j + 4;
+			j += 4;
 		}
 		i++;
 
 	}
 
+}
+
+int relX( int x ){
+	return x;
+}
+
+int relY( int y ){
+	return y/4;
+}
+
+int absX( int x ){
+	return x;
+}
+
+int absY( int y ){
+	return y*4;
 }
