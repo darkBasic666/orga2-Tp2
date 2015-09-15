973e69645c5900d6b01c011314be6259e6127526
Maximiliano Leon Paz <m4xileon@gmail.com>
2015-09-15 20:11:48 -0300

    finish blur.

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -4,6 +4,7 @@
 #include "../tp2.h"
 
 
+
 void blur_c    (
     unsigned char *src,
     unsigned char *dst,
@@ -15,48 +16,84 @@ void blur_c    (
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
-    int i = radius;
+    float k [(2 * radius ) + 1][(2 * radius ) + 1] ;
+    int i = 0;
+    float total= 0;
+    while( i<= (2 * radius ) ){
+    	int j = 0;
+    	while( j <= (2 * radius )  ){
+    		float x = radius -i;
+    		float y = radius - j;
+    		float ePowBla = pow( 2.71828182846  , -1.0 * ( ( (x*x) + (y*y) ) / (2.0*(pow(sigma,2.0))) )  );
+			float constante = (1.0/ (2.0*( 3.14159265359)* pow(sigma,2.0) ));
+    		k[i][j]  =  constante *  ePowBla;
+
+    		// printf("%f | ", k[i][j] );
+    		
+    		total += k[i][j];
+    		// printf ("k[i][j] :%f \n" , k[i][j]);
+    		j++;
+    	}	
+    	// printf("\n" );
+    	i++;
+    }
+    // printf("total : %f \n" , total);
+
+
+    i = radius;
 	while(i< (filas - radius)){
 		// printf ("i :%i \n" , i);
-		int j = radius*4;
-		while(j < (cols -radius) *4 ){
+		int j = radius;
+		while(j < (cols -radius)  ){
 			// printf ("j :%i \n" , j);
-			int promedio_r = 0;
-			int promedio_g = 0;
-			int promedio_b = 0;
+			float promedio_r = 0;
+			float promedio_g = 0;
+			float  promedio_b = 0;
 			int aux_i = i - radius;
-			int aux_j = j - radius*4;
+			int x =  0;
+			//while(aux_i <= (i + 2*radius) ){
 			while(aux_i <= (i + radius) ){
-				//int aux_j = j - radius*4;
-				while(aux_j <= ( j + radius*4 ) ) {
-					bgra_t *src_pixel = (bgra_t*)&src_matrix[aux_i][aux_j];
-					promedio_r += src_pixel->r;
-					promedio_g += src_pixel->g;
-					promedio_b += src_pixel->b;
+				int aux_j = j - radius;
+				int y = 0;
+				//while(aux_j <= ( j + 2*radius ) ) {
+				while(aux_j <= ( j + radius ) ) {
+					bgra_t *src_pixel = (bgra_t*)&src_matrix[aux_i][aux_j *4];
+					
+					promedio_r += ((float)src_pixel->r * k[x][y]);
+					promedio_g +=  ((float)src_pixel->g * k[x][y]);
+					promedio_b +=  ((float)src_pixel->b * k[x][y]);
 					//aux_j = aux_j + 4;
-					aux_j += 4;
+					aux_j ++;
+					y ++ ;
 				}
 				aux_i++;
+				x ++;
 			}
-			promedio_r = promedio_r / (radius*radius);
-			promedio_g = promedio_g / (radius*radius) ;
-			promedio_b = promedio_b / (radius*radius) ;
+			// promedio_r = promedio_r / ((2*radius)+1)*((2*radius)+1);
+			// promedio_g = promedio_g / ((2*radius)+1)*((2*radius)+1);
+			// promedio_b = promedio_b / ((2*radius)+1)*((2*radius)+1);
 
-			bgra_t *dst_pixel = (bgra_t*)&dst_matrix[i][j];
+			bgra_t *dst_pixel = (bgra_t*)&dst_matrix[i][j*4];
 			
-
-			dst_pixel->r = promedio_r;
-			dst_pixel->g = promedio_g;
-			dst_pixel->b = promedio_b;
-			dst_pixel->a = 255;
+			dst_pixel->r = (unsigned char) promedio_r;
+			dst_pixel->g = (unsigned char) promedio_g;
+			dst_pixel->b = (unsigned char) promedio_b;
+			// dst_pixel->a = 255;
+			// printf ("(%i , %i , %i ) | " ,promedio_r , promedio_g,promedio_b);
+			// printf ("dst_pixel->r :%i ," ,promedio_r);
+			// printf ("dst_pixel->g :%i ," , promedio_b);
+			// printf ("dst_pixel->b :%i \n" , promedio_g);
 			//j = j + 4;
-			j += 4;
+			j ++;
 		}
+		// printf("\n" );
 		i++;
 
 	}
 
 }
+
+
 
 int relX( int x ){
 	return x;
--- filtros/diff_c.c
+++ filtros/diff_c.c
@@ -24,11 +24,11 @@ void diff_c (
 	while(i<n){
 		// printf ("i :%i \n" , i);
 		int j = 0;
-		while(j < (m*4)){
+		while(j < (m)){
 			// printf ("j :%i \n" , j);
-			bgra_t *dst_pixel = (bgra_t*)&dst_matrix[i][j];
-			bgra_t *src_pixel = (bgra_t*)&src_matrix[i][j];
-			bgra_t *src_2_pixel = (bgra_t*)&src_2_matrix[i][j];
+			bgra_t *dst_pixel = (bgra_t*)&dst_matrix[i][j*4];
+			bgra_t *src_pixel = (bgra_t*)&src_matrix[i][j*4];
+			bgra_t *src_2_pixel = (bgra_t*)&src_2_matrix[i][j*4];
 
 			dst_pixel->r = abs(src_pixel->r - src_2_pixel->r);
 			dst_pixel->g = abs(src_pixel->g - src_2_pixel->g);
@@ -37,9 +37,7 @@ void diff_c (
 			j++;
 		}
 		i++;
-		// src_matrix = (unsigned char (*)[(i+ 1) * src_row_size]) src;
-		// src_2_matrix = (unsigned char (*)[(i+ 1) * src_2_row_size]) src_2;
-		// dst_matrix = (unsigned char (*)[(i+ 1) * dst_row_size]) dst;
+
 	}
 }
 
